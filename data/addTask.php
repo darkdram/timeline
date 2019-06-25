b785009ec316eb2d76e3b5d64f1b3de06ff69697
Vladimir <bakzzzbunny@gmail.com>
2019-06-25 15:32:25 +0300

    remove datepicker, change vue-datepicker and multiselect. change task titles in add task method. add localization to visjs

--- data/addTask.php
+++ data/addTask.php
@@ -18,19 +18,33 @@ $project_contract_work_end         = '1971-08-18';
 $project_contract_report_start     = '1971-08-19';
 $project_contract_report_end       = '1971-08-19';
 
+
+$taskNames = array(
+  'real' => array(
+    'admittance' => 'Допуск',
+    'work' => 'Реальные сроки проведения работ',
+    'report' => 'реальные сроки сдачи технических отчетов'
+  ),
+  'contract' => array(
+    'admittance' => 'Допуск',
+    'work' => 'Договорные сроки проведения работ',
+    'report' => 'Договорные сроки сдачи технических отчетов'
+  )
+);
+
 $dbh->beginTransaction();
 $query = "INSERT INTO projects ( `content` ) VALUES ( '{$project_name}' )";
 $res = $dbh->query( $query );
 $project_id = $dbh->lastInsertId();
 
 
-$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( '{$project_name}', '{$project_id}', '{$project_contract_start}',            '{$project_contract_end}',            '{$project_id}', 'background', 'contract' )");
-$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( 'Допуск',          '{$project_id}', '{$project_real_admittance_start}',     '{$project_real_admittance_end}',     '{$project_id}', 'range',      'real'     )");
-$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( 'Допуск',          '{$project_id}', '{$project_contract_admittance_start}', '{$project_contract_admittance_end}', '{$project_id}', 'range',      'contract' )");
-$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( 'Работа',          '{$project_id}', '{$project_real_work_start}',           '{$project_real_work_end}',           '{$project_id}', 'range',      'real'     )");
-$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( 'Работа',          '{$project_id}', '{$project_contract_work_start}',       '{$project_contract_work_end}',       '{$project_id}', 'range',      'contract' )");
-$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( 'Отчет' ,          '{$project_id}', '{$project_real_report_start}',         '{$project_real_report_end}',         '{$project_id}', 'range',      'real'     )");
-$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( 'Отчет' ,          '{$project_id}', '{$project_contract_report_start}',     '{$project_contract_report_end}',     '{$project_id}', 'range',      'contract' )");
+$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( '{$project_name}',                        '{$project_id}', '{$project_contract_start}',            '{$project_contract_end}',            '{$project_id}', 'background', 'contract' )");
+$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( '{$taskNames['real']['admittance']}',     '{$project_id}', '{$project_real_admittance_start}',     '{$project_real_admittance_end}',     '{$project_id}', 'range',      'real'     )");
+$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( '{$taskNames['contract']['admittance']}', '{$project_id}', '{$project_contract_admittance_start}', '{$project_contract_admittance_end}', '{$project_id}', 'range',      'contract' )");
+$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( '{$taskNames['real']['work']}',           '{$project_id}', '{$project_real_work_start}',           '{$project_real_work_end}',           '{$project_id}', 'range',      'real'     )");
+$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( '{$taskNames['contract']['work']}',       '{$project_id}', '{$project_contract_work_start}',       '{$project_contract_work_end}',       '{$project_id}', 'range',      'contract' )");
+$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( '{$taskNames['real']['report']}' ,        '{$project_id}', '{$project_real_report_start}',         '{$project_real_report_end}',         '{$project_id}', 'range',      'real'     )");
+$dbh->query("INSERT INTO timetable ( `content`, `id_project`, `start`, `end`, `group`, `type`, `subtype` ) VALUES ( '{$taskNames['contract']['report']}' ,    '{$project_id}', '{$project_contract_report_start}',     '{$project_contract_report_end}',     '{$project_id}', 'range',      'contract' )");
 
 $dbh->commit();
 
